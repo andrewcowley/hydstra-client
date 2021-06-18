5e818e2ec6d3f2caa34694adfeb24d68ef4b8ccf
Andrew Cowley <andrew@andrewcowley.net>
2021-06-19 00:19:03 +1000

    Add try/catch to makeRequest

--- index.js
+++ index.js
@@ -5,21 +5,24 @@ class HydstraClient {
     this.baseURL = options.baseURL;
   }
 
-    async getSiteList (queryParams) {
-      const requestURL = this.buildURL(queryParams);
-      const response = await this.makeRequest(requestURL);
-      return response;
+  async getSiteList(queryParams) {
+    const requestURL = this.buildURL(queryParams);
+    const response = await this.makeRequest(requestURL);
+    return response;
   }
 
   buildURL(queryParams) {
-    const requestURL =  `${this.baseURL}${JSON.stringify(queryParams)}&ver=2`;
+    const requestURL = `${this.baseURL}${JSON.stringify(queryParams)}&ver=2`;
     return requestURL;
   }
 
   async makeRequest(url) {
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
-
