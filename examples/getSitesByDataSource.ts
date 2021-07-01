2a3754cc616fa8fb461bc0da1a0950ca3df21d55
Andrew Cowley <andrew@andrewcowley.net>
2021-07-01 22:24:02 +1000

    Add getSitesByDataSource example

---
+++ examples/getSitesByDataSource.ts
@@ -0,0 +1,5 @@
+import { HydstraClient } from "../src";
+
+const hc = new HydstraClient({baseURL: 'https://realtimedata.waternsw.com.au/cgi/webservice.exe'});
+
+hc.getSitesByDataSource({datasources: ["CP"]}).then((response) => {console.log(response)})
