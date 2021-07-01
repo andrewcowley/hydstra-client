1a70919b27b19b512c8f67477aff897672ea8638
Andrew Cowley <andrew@andrewcowley.net>
2021-07-01 22:28:33 +1000

    Add getSiteGeojson example

---
+++ examples/getSiteGeoJSONExample.ts
@@ -0,0 +1,8 @@
+import { HydstraClient } from "../src";
+
+const hc = new HydstraClient({ baseURL: 'https://realtimedata.waternsw.com.au/cgi/webservice.exe' })
+
+hc.getSiteGeojson({
+    site_list: "204006",
+    fields: ["zone"]
+}).then((response) => {console.log(response)})
